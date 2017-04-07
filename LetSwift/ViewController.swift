d8a4e9eb775b1dafd0966f113bc6710dc1906a6f
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-07 09:36:12 +0200

    Initial project setup

---
+++ LetSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 07.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
