acfcd5fc7d3482deb3724e9fef237e966df74e7f
Marcin Chojnacki <marcinch7@gmail.com>
2017-04-10 15:26:49 +0200

    Add BaseViewController with automatic localization

---
+++ LetSwift/GenericViewControllers/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 10.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLocalization()
+    }
+    
+    func setupLocalization() { }
+}
