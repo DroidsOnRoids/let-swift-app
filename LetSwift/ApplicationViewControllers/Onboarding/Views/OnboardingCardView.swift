690f2a6b9b097abd8df14825e5494c50470cc5d2
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-13 10:28:25 +0200

    Initial structure of onboarding card with DesignableView

---
+++ LetSwift/ApplicationViewControllers/Onboarding/Views/OnboardingCardView.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingCardView.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki, Kinga Wilczek on 13.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingCardView: DesignableView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
