a3048be71bb51cbc87aee3e8b4bd96f2ef500b13
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-21 09:18:31 +0200

    Add empty view controllers for tabs

---
+++ LetSwift/ApplicationViewControllers/Contact/ContactViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactViewController.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 21.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+final class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ LetSwift/ApplicationViewControllers/Speakers/SpeakersViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SpeakersViewController.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 21.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+final class SpeakersViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
