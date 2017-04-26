a70dd6b38de02140b980cf518faef60ee72f5fda
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-26 22:00:51 +0200

    Add event time cell view

---
+++ LetSwift/ApplicationViewControllers/Events/Views/EventTimeCell/EventTimeCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventTimeCell.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 26.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+final class EventTimeCell: UITableViewCell {
+    
+}
