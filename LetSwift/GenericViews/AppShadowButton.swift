7d91c12cb6c471805429d1982e10cce9ee8206ea
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-26 20:04:10 +0200

    Improve AppShadowButton to reuse it later

--- LetSwift/GenericViews/AppShadowButton.swift
+++ LetSwift/GenericViews/AppShadowButton.swift
@@ -2,7 +2,7 @@
 //  AppShadowButton.swift
 //  LetSwift
 //
-//  Created by Kinga Wilczek on 20.04.2017.
+//  Created by Kinga Wilczek, Marcin Chojnacki on 20.04.2017.
 //  Copyright © 2017 Droids On Roids. All rights reserved.
 //
 
@@ -10,19 +10,59 @@ import UIKit
 
 final class AppShadowButton: UIButton {
     
+    private enum Constants {
+        static let shadowHorizontalInset: CGFloat = 10.0
+        static let shadowHeight: CGFloat = 20.0
+    }
+    
+    override var bounds: CGRect {
+        didSet {
+            if shadowVisible {
+                updateShadowPath()
+            }
+        }
+    }
+    
     override var isHighlighted: Bool {
         didSet {
             let onNormal = {
-                self.showShadow()
+                if self.shadowVisible {
+                    self.layer.shadowOpacity = 1.0
+                }
+                
                 self.transform = .identity
             }
             
             let onHighlighted = {
-                self.hideShadow()
+                if self.shadowVisible {
+                    self.layer.shadowOpacity = 0.0
+                }
+                
                 self.transform = CGAffineTransform(translationX: 0.0, y: 2.0)
             }
             
             isHighlighted ? onHighlighted() : onNormal()
         }
+    }
+    
+    @IBInspectable
+    var shadowVisible: Bool = false {
+        didSet {
+            if shadowVisible {
+                layer.shadowColor = backgroundColor?.cgColor
+                layer.shadowOpacity = 1.0
+                layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+                layer.shadowRadius = 10.0
+                
+                updateShadowPath()
+            } else {
+                layer.shadowOpacity = 0.0
+            }
+        }
+    }
+    
+    private func updateShadowPath() {
+        let shadowRect = CGRect(x: Constants.shadowHorizontalInset, y: bounds.height - Constants.shadowHeight, width: bounds.width - Constants.shadowHorizontalInset * 2, height: Constants.shadowHeight)
+        layer.shadowPath = UIBezierPath(rect: shadowRect).cgPath
     }
 }
