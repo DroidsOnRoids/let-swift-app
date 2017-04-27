98fe636dcbe6eb81694f11450077b5592e6d63f5
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-04-27 16:30:19 +0200

    Initial concept of data source proxy

--- LetSwift/ApplicationViewControllers/Events/EventsViewController.swift
+++ LetSwift/ApplicationViewControllers/Events/EventsViewController.swift
@@ -23,10 +23,15 @@ final class EventsViewController: AppViewController {
         super.viewDidLoad()
 
         setup()
+        tableView.estimatedRowHeight = 90
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(nibName: "StaticImageTableViewCell", bundle: nil), forCellReuseIdentifier: "StaticImageTableViewCell")
 
         let bindableArray = ["Cos", "Cos", "nic", "Ale działa"].bindable
-        bindableArray.bind(to: tableView.item(with: "sth", cellType: UITableViewCell.self) ({ index, item, cell in
+        bindableArray.bind(to: tableView.item(with: "StaticImageTableViewCell", cellType: UITableViewCell.self) ({ index, item, cell in
             print(index, item, cell)
+
         }))
 
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
@@ -50,22 +55,6 @@ final class EventsViewController: AppViewController {
     }
 
     private func setup() {
-        setupDelegates()
-    }
-
-    private func setupDelegates() {
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-}
-
-extension EventsViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
     }
 }
 
@@ -113,31 +102,94 @@ extension UITableView {
             return { cellFormer in
                 return { source in
                     return { index, item in
-                        print(source)
-                        //                    let cell = UITableView().dequeueReusableCell(withIdentifier: "Sth", for: IndexPath(row: index, section: 0)) as! Cell
-                        let cell = UITableViewCell() as! Cell
-                        cellFormer(index, item, cell)
+                        let delegate = ReactiveTableViewDataSource<S> { tableView, index, element in
+                            let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: IndexPath(row: index, section: 0)) as! Cell
+                            cellFormer(index, item, cell)
+                            return cell
+                        }
+                        delegate.tableView(self, observedElements: source) //maybe here proxy is needed
                     }
                 }
             }
     }
+
+    func createRxDataSourceProxy() -> ReactiveTableViewDataSourceProxy {
+        return ReactiveTableViewDataSourceProxy()
+    }
 }
 
-final class ReactiveTableViewObservable<S: Sequence>: NSObject, UITableViewDelegate, UITableViewDataSource {
+final class ReactiveTableViewDataSource<S: Sequence>: NSObject, UITableViewDataSource {
 
-    typealias CellFormer = (Int, UITableViewCell, S.Iterator.Element) -> UITableViewCell
+    typealias CellFormer = (UITableView, Int, S.Iterator.Element) -> UITableViewCell
 
-    private var cellFormer: CellFormer?
+    private var cellFormer: CellFormer
+
+    private var items: [S.Iterator.Element] = []
 
     init(cellFormer: @escaping CellFormer) {
         self.cellFormer = cellFormer
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return cellFormer(tableView, indexPath.item, items[indexPath.row])
+    }
+
+    //Needs to reimplement
+    func tableView(_ tableView: UITableView, observedElements: S) {
+        items = observedElements.map({ $0 })
+        print(items)
+        tableView.reloadData()
+    }
+}
+
+final class ReactiveTableViewDataSourceProxy: NSObject, UITableViewDataSource {
+
+    fileprivate weak var dataSourceMethods: UITableViewDataSource?
+
+//    public static func proxyForObject(_ object: UITableView) -> Self {
+//
+//        let maybeProxy = Self.assignedProxyFor(object) as? Self
+//
+//        let proxy: Self
+//        if let existingProxy = maybeProxy {
+//            proxy = existingProxy
+//        }
+//        else {
+//            proxy = Self.createProxy(for: object) as! Self
+//            Self.assignProxy(proxy, toObject: object)
+//            assert(Self.assignedProxyFor(object) === proxy)
+//        }
+//
+//        let currentDelegate: AnyObject? = Self.currentDelegateFor(object)
+//
+//        if currentDelegate !== proxy {
+//            proxy.setForwardToDelegate(currentDelegate, retainDelegate: false)
+//            assert(proxy.forwardToDelegate() === currentDelegate)
+//            Self.setCurrentDelegate(proxy, toObject: object)
+//            assert(Self.currentDelegateFor(object) === proxy)
+//            assert(proxy.forwardToDelegate() === currentDelegate)
+//        }
+//        
+//        return proxy
+//    }
+
+    class func createProxy(for tableView: UITableView) -> AnyObject {
+        return tableView.createRxDataSourceProxy()
+    }
+
+    class func setCurrentDelegate(_ delegate: UITableViewDataSource?, to tableView: UITableView) {
+        tableView.dataSource = delegate
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSourceMethods?.tableView(tableView, numberOfRowsInSection: section) ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return dataSourceMethods?.tableView(tableView, cellForRowAt: indexPath) ?? UITableViewCell()
     }
 }
