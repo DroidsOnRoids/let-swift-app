d8080763032a95a05d700f6420f3c6ab394eac07
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-04-30 19:17:07 +0200

    Fix content touch delay

---
+++ LetSwift/Helpers/Extensions/UITableViewExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UITableViewExtension.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 30.04.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    override open var delaysContentTouches: Bool {
+        didSet {
+            changeChildDelaysContentTouches()
+        }
+    }
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        
+        changeChildDelaysContentTouches()
+    }
+    
+    private func changeChildDelaysContentTouches() {
+        for case let child as UIScrollView in subviews {
+            child.delaysContentTouches = delaysContentTouches
+        }
+    }
+}
