df2640ceca81bc5fae4edeeadf77de70880dcbce
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-05-09 11:00:35 +0200

    Initial files for carousel cell

---
+++ LetSwift/ApplicationViewControllers/Events/Views/CarouselEventPhotosCell/CarouselEventPhotosCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarouselEventPhotosCell.swift
+//  LetSwift
+//
+//  Created by Kinga Wilczek on 09.05.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class CarouselEventPhotosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
