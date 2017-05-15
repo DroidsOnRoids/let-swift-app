057abb1ad21f6e6e3c3d19f53b823db87432b27d
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-05-15 17:19:05 +0200

    Initial structure of PhotosViewController

---
+++ LetSwift/ApplicationViewControllers/Photos/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  LetSwift
+//
+//  Created by Kinga Wilczek on 15.05.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+}
