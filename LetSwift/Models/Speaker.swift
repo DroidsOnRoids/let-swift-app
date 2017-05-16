8f2d4eb84d0765d9e35f7194bd7accd6bdf44056
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-05-16 08:39:39 +0200

    Speaker model

---
+++ LetSwift/Models/Speaker.swift
@@ -0,0 +1,18 @@
+//
+//  Speaker.swift
+//  LetSwift
+//
+//  Created by Kinga Wilczek on 16.05.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import Foundation
+
+struct Speaker {
+
+    let id: Int?
+    let avatar: Photo?
+    let name: String?
+    let job: String?
+    let bio: String?
+}
