9916e4c870b4da8e199ecbd397f0348c680c1e9a
Marcin Chojnacki <marcinch7@gmail.com>
2017-06-01 12:09:43 +0200

    Implement localizations & UX

--- LetSwift/ApplicationViewControllers/Contact/ContactViewController.swift
+++ LetSwift/ApplicationViewControllers/Contact/ContactViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 final class ContactViewController: AppViewController {
     
-    @IBOutlet private weak var topicButton: UIButton!
-    @IBOutlet private weak var nameTextField: AppTextField!
-    @IBOutlet private weak var emailTextField: AppTextField!
-    @IBOutlet private weak var messageTextView: UITextView!
+    @IBOutlet fileprivate weak var topicButton: UIButton!
+    @IBOutlet fileprivate weak var nameTextField: AppTextField!
+    @IBOutlet fileprivate weak var emailTextField: AppTextField!
+    @IBOutlet fileprivate weak var messageTextView: UITextView!
+    @IBOutlet fileprivate weak var sendButton: AppShadowButton!
     
     override var viewControllerTitleKey: String? {
         return "CONTACT_TITLE"
@@ -23,7 +24,9 @@ final class ContactViewController: AppViewController {
         return true
     }
     
-    var viewModel: ContactViewControllerViewModel!
+    private var viewModel: ContactViewControllerViewModel!
+    
+    fileprivate var messagePlaceholder: String!
 
     convenience init(viewModel: ContactViewControllerViewModel) {
         self.init()
@@ -32,7 +35,13 @@ final class ContactViewController: AppViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
         setupViews()
+        setupTouchToDismiss()
+        setupLocalization()
     }
     
     private func setupViews() {
@@ -41,7 +50,40 @@ final class ContactViewController: AppViewController {
         topicButton.layer.borderColor = borderColor
         nameTextField.layer.borderColor = borderColor
         emailTextField.layer.borderColor = borderColor
+        
+        messageTextView.delegate = self
         messageTextView.layer.borderColor = borderColor
         messageTextView.textContainerInset = UIEdgeInsets(top: 16.0, left: 12.0, bottom: 16.0, right: 12.0)
+    }
+    
+    private func setupTouchToDismiss() {
+        let recognizer = UITapGestureRecognizer(target: self.view, action: #selector(view.endEditing(_:)))
+        recognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(recognizer)
+    }
+}
+
+extension ContactViewController: Localizable {
+    func setupLocalization() {
+        topicButton.setTitle(localized("CONTACT_TOPIC"), for: [])
+        nameTextField.placeholder = localized("CONTACT_NAME")
+        emailTextField.placeholder = localized("CONTACT_EMAIL")
+        messagePlaceholder = localized("CONTACT_MESSAGE")
+        sendButton.setTitle(localized("CONTACT_SEND").uppercased(), for: [])
+    }
+}
+
+// TODO: Replace this with something better
+extension ContactViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == messagePlaceholder {
+            textView.text = ""
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = messagePlaceholder
+        }
     }
 }
