45d3019383d934aef71792db0624199ffc5b90de
Marcin Chojnacki <marcinch7@gmail.com>
2017-06-06 11:17:27 +0200

    Working photo list

--- LetSwift/ApplicationViewControllers/Photos/PhotoGalleryViewController.swift
+++ LetSwift/ApplicationViewControllers/Photos/PhotoGalleryViewController.swift
@@ -11,5 +11,58 @@ import UIKit
 protocol PhotoGalleryViewControllerDelegate: class {
 }
 
-final class PhotoGalleryViewController: UIViewController {
+final class PhotoGalleryViewController: AppViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    override var viewControllerTitleKey: String? {
+        return "PHOTOS_TITLE"
+    }
+    
+    override var shouldHideShadow: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.registerCells([SinglePhotoCell.self])
+        collectionView.delegate = self
+        
+        [0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5]
+            .bindable.bind(to: collectionView.items() ({ collectionView, index, element in
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SinglePhotoCell.cellIdentifier, for: indexPath) as! SinglePhotoCell
+            
+            return cell
+            
+            /*if let event = element {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PreviousEventCell.cellIdentifier, for: indexPath) as! PreviousEventCell
+                cell.title = event.title
+                cell.date = event.date?.stringDateValue
+                
+                return cell
+            } else {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LoadingCollectionViewCell.cellIdentifier, for: indexPath) as! LoadingCollectionViewCell
+                cell.animateSpinner()
+                
+                return cell
+            }*/
+        }))
+    }
+}
+
+extension PhotoGalleryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing = (collectionViewLayout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing ?? 0.0
+        let cellPadding = itemSpacing * CGFloat(columnNumber + 1)
+        let cellSize = (collectionView.bounds.width - cellPadding) / CGFloat(columnNumber)
+        
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
+    private var columnNumber: Int {
+        return DeviceScreenHeight.deviceHeight > DeviceScreenHeight.inch4¨7.rawValue ? 3 : 2
+    }
 }
---
+++ LetSwift/ApplicationViewControllers/Photos/Views/SinglePhotoCell/SinglePhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  SinglePhotoCell.swift
+//  LetSwift
+//
+//  Created by Marcin Chojnacki on 06.06.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+import UIKit
+
+class SinglePhotoCell: UICollectionViewCell {
+    
+    static let cellIdentifier = String(describing: SinglePhotoCell.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
