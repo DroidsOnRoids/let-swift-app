e81b2b92649077f4027b914b931b6aa4888e05c1
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-06-16 15:02:17 +0200

    Mock loader improvements

--- LetSwift/Models/Mocks/MockLoader.swift
+++ LetSwift/Models/Mocks/MockLoader.swift
@@ -10,30 +10,22 @@ import Foundation
 
 // MARK: Remove when networking layer will be available
 struct MockLoader {
-    
-    static func loadMock(forPath path: String) -> NSDictionary? {
+
+    static func loadMock<T>(forPath path: String) -> T? {
         guard let file = Bundle.main.url(forResource: path, withExtension: "json"),
             let data = try? Data(contentsOf: file),
             let json = try? JSONSerialization.jsonObject(with: data, options: []) else { return nil }
 
-        return json as? NSDictionary
-    }
-
-    static func loadArrayMock(forPath path: String) -> NSArray? {
-        guard let file = Bundle.main.url(forResource: path, withExtension: "json"),
-            let data = try? Data(contentsOf: file),
-            let json = try? JSONSerialization.jsonObject(with: data, options: []) else { return nil }
-
-        return json as? NSArray
+        return json as? T
     }
     
     static var eventDetailsMock: NSDictionary? {
-        guard let result = loadMock(forPath: "event_details") else { return nil }
+        guard let result: NSDictionary? = loadMock(forPath: "event_details") else { return nil }
         return result
     }
 
     static var speakersMock: NSArray? {
-        guard let result = loadArrayMock(forPath: "speakers") else { return nil }
+        guard let result: NSArray? = loadMock(forPath: "speakers") else { return nil }
         return result
     }
 }
