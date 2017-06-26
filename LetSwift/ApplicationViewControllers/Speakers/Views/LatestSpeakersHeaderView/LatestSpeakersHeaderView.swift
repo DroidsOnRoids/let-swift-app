2de959b51b04b93fe9de490e116c5687e710c3df
Marcin Chojnacki <marcinch7@gmail.com>
2017-06-26 15:49:06 +0200

    Fix cell aspect ratio

--- LetSwift/ApplicationViewControllers/Speakers/Views/LatestSpeakersHeaderView/LatestSpeakersHeaderView.swift
+++ LetSwift/ApplicationViewControllers/Speakers/Views/LatestSpeakersHeaderView/LatestSpeakersHeaderView.swift
@@ -22,7 +22,7 @@ class LatestSpeakersHeaderView: DesignableView {
     }
 
     private func setup() {
-        latestCollectionView.collectionViewLayout = AppCollectionViewFlowLayout(with: 180.0)
+        latestCollectionView.delegate = self
         latestCollectionView.registerCells([LatestSpeakerCollectionViewCell.self])
 
         latestSpeakersTitleLabel.text = localized("SPEAKERS_LATEST_TITLE").uppercased()
@@ -35,5 +35,11 @@ class LatestSpeakersHeaderView: DesignableView {
         Array(mockedSpeakers[0..<5]).bindable.bind(to: latestCollectionView.item(with: LatestSpeakerCollectionViewCell.cellIdentifier, cellType: LatestSpeakerCollectionViewCell.self) ({ index, element, cell in
             cell.load(with: element)
         }))
+    }
+}
+
+extension LatestSpeakersHeaderView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width * 0.4, height: collectionView.bounds.height)
     }
 }
