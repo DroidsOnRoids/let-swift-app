5b221198133c049270c3d2a551c22dbdc646459e
Kinga Wilczek <kinga.wilczek@droidsonroids.pl>
2017-07-12 12:58:39 +0200

    Initial state of CI appStore and release build

---
+++ LetSwift/Enums/AppCompilationConditions.swift
@@ -0,0 +1,11 @@
+//
+//  AppCompilationConditions.swift
+//  LetSwift
+//
+//  Created by Kinga Wilczek on 12.07.2017.
+//  Copyright © 2017 Droids On Roids. All rights reserved.
+//
+
+enum AppCompilationConditions {
+    case debug, appStore, release
+}
