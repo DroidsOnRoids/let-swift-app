728309ec2e3f076014f2e5810e8423565542e5c8
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-09-29 12:24:36 +0200

    [LSI-316] Support both absolute and relative API photo path

--- LetSwift/Helpers/Extensions/URLExtension.swift
+++ LetSwift/Helpers/Extensions/URLExtension.swift
@@ -21,8 +21,26 @@
 import Foundation
 
 extension URL {
+    private enum Constants {
+        static let convertHttpToHttps = true
+        static let httpPrefix = "http://"
+        static let httpsPrefix = "https://"
+    }
+    
     static var baseServerURL: URL? {
         guard let baseURL = Bundle.main.infoDictionary?["BaseServerURL"] as? String else { return nil }
         return try? baseURL.asURL()
+    }
+    
+    static func from(modelURL urlString: String?) -> URL? {
+        guard let urlString = urlString else { return nil }
+        
+        if urlString.starts(with: Constants.httpsPrefix) {
+            return try? urlString.asURL()
+        } else if urlString.starts(with: Constants.httpPrefix) && Constants.convertHttpToHttps {
+            return try? urlString.replacingOccurrences(of: Constants.httpPrefix, with: Constants.httpsPrefix).asURL()
+        } else {
+            return URL.baseServerURL?.appendingPathComponent(urlString)
+        }
     }
 }
