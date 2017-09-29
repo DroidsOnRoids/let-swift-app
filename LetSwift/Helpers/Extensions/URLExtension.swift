57748751beb1352f08488f4eddfe0cd20baed839
Marcin Chojnacki <marcin.chojnacki@droidsonroids.pl>
2017-09-29 12:44:49 +0200

    [LSI-316] Build fix

--- LetSwift/Helpers/Extensions/URLExtension.swift
+++ LetSwift/Helpers/Extensions/URLExtension.swift
@@ -35,9 +35,9 @@ extension URL {
     static func from(modelURL urlString: String?) -> URL? {
         guard let urlString = urlString else { return nil }
         
-        if urlString.starts(with: Constants.httpsPrefix) {
+        if urlString.hasPrefix(Constants.httpsPrefix) {
             return try? urlString.asURL()
-        } else if urlString.starts(with: Constants.httpPrefix) && Constants.convertHttpToHttps {
+        } else if urlString.hasPrefix(Constants.httpPrefix) && Constants.convertHttpToHttps {
             return try? urlString.replacingOccurrences(of: Constants.httpPrefix, with: Constants.httpsPrefix).asURL()
         } else {
             return URL.baseServerURL?.appendingPathComponent(urlString)
